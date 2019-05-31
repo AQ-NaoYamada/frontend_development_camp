19343e0cbcdd8938a79962c1b6ad48750e6b9846
NaoYamada0201 <nao.yamada@asia-quest.jp>
2019-05-31 18:52:01 +0900

    5/31 作業分追加

--- js/frontend_development_camp.js
+++ js/frontend_development_camp.js
@@ -13,4 +13,36 @@ $(function(){
        $('body,html').animate({scrollTop:position}, speed, 'swing');
        return false;
     });
+
  });
+
+// function checkBelongings() {
+//    var belongingsList = []; // 選択されているか否かを判定する変数
+    
+//    for (var i = 0; i < document.form1.check.length; i++) {
+    
+//        // i番目のチェックボックスがチェックされているかを判定
+//        if (document.form1.check[i].checked) {
+//          belongingsList.push(document.form1.check[i].value);    
+//        }
+//        if (belongingsList.length === 7) {
+//          document.getElementById('ok').innerText = '持ち物OK！'
+//        }
+//    }
+// }
+
+$(function(){
+  $('input[name="check"]').on('change', function(){
+    /// チェックされたvalue値を配列として取得
+    var belongingsList = $('input[name="check"]:checked').map(function(){
+      return $(this).val();
+    }).get();
+    if (belongingsList.length === 7) {
+      // document.write('<img src="./images/good.png" alt="good" width=10% height=10%>')
+      var ok = document.getElementById('ok');
+      ok.innerHTML = '<img src="./images/good.png" alt="good" width=80px height=80px>';
+   }else {
+      document.getElementById('ok').innerText = ''
+   };
+   })
+});
