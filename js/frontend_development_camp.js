3d8f7d2226c624199882cd20d8ce6f3ea1709fcf
NaoYamada0201 <nao.yamada@asia-quest.jp>
2019-06-03 18:35:55 +0900

    6/3 作業分追加

--- js/frontend_development_camp.js
+++ js/frontend_development_camp.js
@@ -16,21 +16,6 @@ $(function(){
 
  });
 
-// function checkBelongings() {
-//    var belongingsList = []; // 選択されているか否かを判定する変数
-    
-//    for (var i = 0; i < document.form1.check.length; i++) {
-    
-//        // i番目のチェックボックスがチェックされているかを判定
-//        if (document.form1.check[i].checked) {
-//          belongingsList.push(document.form1.check[i].value);    
-//        }
-//        if (belongingsList.length === 7) {
-//          document.getElementById('ok').innerText = '持ち物OK！'
-//        }
-//    }
-// }
-
 $(function(){
   $('input[name="check"]').on('change', function(){
     /// チェックされたvalue値を配列として取得
@@ -38,7 +23,6 @@ $(function(){
       return $(this).val();
     }).get();
     if (belongingsList.length === 7) {
-      // document.write('<img src="./images/good.png" alt="good" width=10% height=10%>')
       var ok = document.getElementById('ok');
       ok.innerHTML = '<img src="./images/good.png" alt="good" width=80px height=80px>';
    }else {
